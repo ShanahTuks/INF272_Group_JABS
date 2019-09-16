e6fdd060244d8e02dc98a8aa3c03954e3105fd3c
Shanah Suping Jr <u18181539@tuks.co.za>
2019-09-16 10:31:45 +0200

    Copying over data from Master Branch

---
+++ 272JABSProject/Controllers/FitnessController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _272JABSProject.Controllers
+{
+    public class FitnessController : Controller
+    {
+        // GET: Fitness
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //<<<<<<< HEAD
+        //<<<<<<< HEAD
+        //<<<<<<< HEAD
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+        //<<<<<<< HEAD
+        //=======
+
+        public ActionResult FoodAnalysis()
+        {
+            return View();
+        }
+        //>>>>>>> Jackie'sBranch
+        //=======
+        public ActionResult ActivityInput()
+        {
+            return View();
+        }
+
+        public ActionResult FoodInput()
+        {
+            return View();
+        }
+        //>>>>>>> brendensbranch
+        //=======
+
+        public ActionResult ActivityAnalysis()
+        {
+            return View();
+        }
+        //>>>>>>> ShanahsBranch
+        //=======
+        public ActionResult LogInPage()
+        {
+
+            return View();
+        }
+        public ActionResult SignUpPage()
+        {
+
+            return View();
+        }
+        //>>>>>>> Amo'sBranch
+    }
+}
